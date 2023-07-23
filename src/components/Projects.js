0bb2b77044d6dca0da9fce5977bd9aaf71fefe6f
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-07-22 22:53:56 -0500

    css in progress

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,9 +3,22 @@ const Projects = (props) => {
 
     //The Components Returned JSX
     return ( <div className="Projects"> 
-        <p> NBA Trivia</p>
-        <p> My first Project <a href="https://sportsflash.onrender.com/"> NBA Trivia.</a> </p> 
-        <p>Link to my<a href="https://sportsflash.onrender.com/"> GitHub</a> for this project</p> 
+        <div className="project1">
+            <div className="image1"> </div>
+            <ul>
+                <li className="title"> NBA Trivia</li>
+                <li> My first Project can be found <a href="https://sportsflash.onrender.com/"> here</a> </li> 
+                <li>Link to my<a href="https://sportsflash.onrender.com/"> GitHub</a> for this project</li> 
+            </ul>
+        </div>
+        <div className="project2">
+            <div className="image2"> </div>
+            <ul>
+                <li className="title"> NBA Trivia</li>
+                <li> My first Project can be found <a href="https://sportsflash.onrender.com/"> here</a> </li> 
+                <li>Link to my<a href="https://sportsflash.onrender.com/"> GitHub</a> for this project</li> 
+            </ul>
+        </div>
     </div> )
 }
 
